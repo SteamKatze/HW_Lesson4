280b60c594dfd77e9bc30500feabf9a18b7a21d4
Svitlana <s.saenko@autodoc.eu>
2023-09-12 11:32:31 +0300

    Check

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ input_string = input("Введіть текст: ")
 # Ініціалізуємо лічильники для літер і цифр
 letter_count = 0
 digit_count = 0
+
+# Аналізуємо символи у рядку
+for char in input_string:
+    # Визначаємо чи є символ літерою або цифрою
+    if char.isalpha():
+        letter_count += 1
+    elif char.isdigit():
+        digit_count += 1
